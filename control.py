48eb1a543d03818ae5142da76b2a3d7cfd71315d
Jack <jwrickman18@bsmschool.org>
2017-12-05 13:02:45 -0600

    More fidgets

--- control.py
+++ control.py
@@ -40,15 +40,15 @@ sensitivity = 500
 def rightMotorScaled(x, y):
         f = int(1000 * y)
         t = int(500 * x)
-        scaled = min(f - t,1000)
-        return int(1500 + scaled)
+        scaled = f - t
+        return int(min(1500 + scaled, 3000))
 
 
 def leftMotorScaled(x, y):
         f = int(1000 * y)
         t = int(500 * x)
-        scaled = min(f + t, 1000)
-        return int(1500 - scaled)
+        scaled = f + t
+        return int(max(1500 - scaled,0))
 
 
 
