755614eebad540ddd596cb06f91926933b63b1fc
Jack <jwrickman18@bsmschool.org>
2017-12-05 15:16:49 -0600

    network dedicated command to work this

--- networkxbox.py
+++ networkxbox.py
@@ -4,3 +4,13 @@ import xbox
 import math
 
 import sys, tty, termios, signal
+
+if __name__ == '__main__':
+    joy = xbox.Joystick()
+
+    while not joy.Back():
+        x, y = joy.leftStick()
+        print x
+        print y
+        xbox.refresh()
+    joy.close()
