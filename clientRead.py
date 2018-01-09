2b6129a4153b52fb7e7a1378d60b5632abc538ba
Andrew Voss <voss1167@gmail.com>
2018-01-08 19:18:40 -0600

    add

---
+++ clientRead.py
@@ -0,0 +1,19 @@
+# --------------------------------File on client--------------------------------
+# Reads
+
+import RoboPiLib as RPL
+import ftplib
+while True:
+    ftp = ftplib.FTP('192.168.1.236', 'avoss19', 'Good!682')
+    ftp.cwd('Desktop/Robot-Controller-Support')
+    gFile = open("ftpTemp", "wb")
+    ftp.retrbinary('RETR ftptestFile.txt', gFile.write)
+    gFile.close()
+    ftp.quit()
+    gFile = open("ftpTemp", "r")
+    buff = gFile.read()
+    convertTxtArray = buff.split()
+    motorL = convertTxtArray[0]
+    motorR = convertTxtArray[1]
+    RPL.servoWrite(0, int(float(motorL))) # for some odd reason would not convert directly to int with int()
+    RPL.servoWrite(1, int(float(motorR))) # for some odd reason would not convert directly to int with int()
