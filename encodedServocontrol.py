a16ede66807e628ef6b4a1c30695b0a2a6833001
Jack <jwrickman18@bsmschool.org>
2018-01-10 09:23:18 -0600

    Fixed

---
+++ encodedServocontrol.py
@@ -0,0 +1,30 @@
+import RoboPiLib as RPL
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+##### Pins #####
+directionPin = 0
+channelAPin = 1
+# VCCPin is attached to pwr for pin 2
+#GNDPin is attached to gnd for pin 2
+channelBPin = 3
+
+
+#### Pin Setup ####
+RPL.pinMode(directionPin, RPL.Output)
+RPL.pinMode(channelAPin, RPL.Input)
+RPL.pinMode(channelBPin, RPL.Input)
+RPL.pinMode(speedPin, RPL.Output)
+#############
+count = 0
+aState = 0
+lastAState = RPL.digitalRead
+
+while True:
+    aState = RPl.digitalRead(channelAPin)
+    if aState != lastAState:
+        if RPL.digitalRead(channelBPin) != aState:
+            count += 1
+        else:
+            count -= 1
+        lastAState = aState
+        print count
