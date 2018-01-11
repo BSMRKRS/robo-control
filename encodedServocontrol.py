359a83252858dc91752adaaf636e3c9419c54cfa
Jack <jwrickman18@bsmschool.org>
2018-01-11 13:04:24 -0600

    Added A run motor function, to run to requested count #

--- encodedServocontrol.py
+++ encodedServocontrol.py
@@ -1,8 +1,8 @@
-import RoboPiLib as RPL
+import RoboPiLib_pwm as RPL
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
 ##### Pins #####
-directionPin = 0
+controlPin = 0
 channelAPin = 1
 # VCCPin is attached to pwr for pin 2
 #GNDPin is attached to gnd for pin 2
@@ -10,21 +10,35 @@ channelBPin = 3
 
 
 #### Pin Setup ####
-RPL.pinMode(directionPin, RPL.Output)
+RPL.pinMode(controlPin, RPL.PWM)
 RPL.pinMode(channelAPin, RPL.Input)
 RPL.pinMode(channelBPin, RPL.Input)
-RPL.pinMode(speedPin, RPL.Output)
 #############
-count = 0
-aState = 0
-lastAState = RPL.digitalRead
+count1 = 0
+a1State = 0
+lastA1State = RPL.digitalRead(channelAPin)
 
-while True:
-    aState = RPl.digitalRead(channelAPin)
-    if aState != lastAState:
-        if RPL.digitalRead(channelBPin) != aState:
-            count += 1
-        else:
-            count -= 1
-        lastAState = aState
-        print count
+def runMotors(newCount1):
+    global count1
+    lastA1State = digitalRead(channelAPin)
+    # Starts Motor1 and Motor2 in correct direction
+    if newCount1 > count1:
+        RPL.pwmWrite(controlPin, 2000, freq)
+    elif newCount1 < count1:
+        RPL.pwmWrite(controlPin, 1000, freq)
+    else:
+        RPL.pwmWrite(controlPin, 1500, freq)
+    # Updates count from encoder
+    while count1 != newCount1
+        a1State = RPl.digitalRead(channelAPin)
+        if a1State != lastA1State: #reads channel a and b from encoder and updates the count (countable events)
+            if RPL.digitalRead(channelBPin) != a1State:
+                count1 += 1
+            else:
+                count1 -= 1
+            lastA1State = a1State
+            print count
+            if count1 == newCount1: #if the current count equals the new count, stop the motor
+                RPL.pwmWrite(controlPin, 1500, freq)
+
+runMotors(5462.22)
