c01bf30aa8867e5f42f71d26cd45eab8388c121a
Jack <jwrickman18@bsmschool.org>
2018-01-25 13:32:15 -0600

    Changed old method to Encoder class method

--- ArmControl.py
+++ ArmControl.py
@@ -66,11 +66,6 @@ RPL.pinMode(motor2ChannelB, RPL.INPUT)
 
 #### GLOBAL VARIABLES ####
 
-#Initializes the count at starting position of arm - need to calculate starting position of angle 2
-global count1
-global count2
-count1 = 0
-count2 = 0
 
 #Length of arms, from axle to axle
 len1 = 12
@@ -135,44 +130,83 @@ def angleToCount(angle, motorXCountableEvents):
 ################################
 
 # Runs motors until count = requested count
-def runMotors(newCount1, newCount2):
-    global count1
-    global count2
-    lastA1State = digitalRead(motor1ChannelA)
-    lastA2State = digitalRead(motor2ChannelA)
+def runMotors(newCount1, newCount2, encoder1, encoder2):
+	global motor1Control, motor2Control, freq
+	count1 = encoder1.Rotary_counter
+	count2 = encoder2.Rotary_counter
     # Starts Motor1 and Motor2 in correct direction
     if newCount1 > count1:
-        RPL.pwmWrite(motor1Control, 2000, freq)
+        RPL.pwmWrite(motor1Control, 2500, freq)
     elif newCount1 < count1:
-        RPL.pwmWrite(motor1Control, 1000, freq)
+        RPL.pwmWrite(motor1Control, 500, freq)
     else:
         RPL.pwmWrite(motor1Control, 1500, freq)
     if newCount2 > count2:
-        RPL.pwmWrite(motor2Control, 2000, freq)
+        RPL.pwmWrite(motor2Control, 2500, freq)
     elif newCount2 < count2:
-        RPL.pwmWrite(motor2Control, 1000, freq)
+        RPL.pwmWrite(motor2Control, 500, freq)
     else:
         RPL.pwmWrite(motor2Control, 1500, freq)
+	a = True
+	b = True
+	while a or b:
+		if abs(newCount1 - encoder1.Rotary_counter) < 5:
+			RPL.pwmWrite(motor1Control, 1500, freq)
+			a = False
+			print "Motor 1 complete"
 
+		if abs(newCount2 - encoder2.Rotary_counter) < 5:
+			RPL.pwmWrite(motor2Control, 1500, freq)
+			b = False
+			print "Motor 2 complete"
     # Updates count from encoder1 and encoder2
-    while count1 != newCount1 or count2 != newCount2:
-        a1State = RPl.digitalRead(motor1ChannelA)
-        if a1State != lastA1State: #reads channel a and b from encoder and updates the count (countable events)
-            if RPL.digitalRead(motor1ChannelB) != a1State:
-                count1 += 1
-            else:
-                count1 -= 1
-            lastA1State = a1State
-            if count1 == newCount1: #if the current count equals the new count, stop the motor
-                RPL.pwmWrite(motor1Control, 1500, freq)
-        if a2State != lastA2State: #reads channel a and b from encoder and updates the count
-            if RPL.digitalRead(motor2ChannelB) != a2State:
-                count2 += 1
-            else:
-                count2 -= 1
-            lastA2State = a2State
-            if count2 == newCount2: #if the current count equals the new count, stop the motor
-                RPL.pwmWrite(motor2Control, 1500, freq)
+
+################################
+	## ENCODER
+################################
+class Encoder(object, Enc_A, Enc_B):
+	global LockRotary
+	def __init__(self):
+		self.Enc_A = Enc_A						#GPIO encoder pin A
+		self.Enc_B = Enc_B							#GPIO encoder pin B
+		self.Current_A = 1						#This assumes that Encoder is initaiate while the motor is not moving
+		self.Current_B = 1
+		self.Rotary_counter = 0
+		self.startEncoders()
+
+	def startEncoders(self):
+		GPIO.setwarnings(True)
+		GPIO.setmode(GPIO.BCM)					# Use BCM mode?
+												# define the Encoder switch inputs ?
+		GPIO.setup(self.Enc_A, GPIO.IN)
+		GPIO.setup(self.Enc_B, GPIO.IN)
+												# setup callback thread for the A and B encoder
+												# use interrupts for all inputs
+		GPIO.add_event_detect(self.Enc_A, GPIO.RISING, callback=self.rotary_interrupt) 				# NO bouncetime
+		GPIO.add_event_detect(self.Enc_B, GPIO.RISING, callback=self.rotary_interrupt) 				# NO bouncetime
+		return
+
+	def rotary_interrupt(self, A_or_B):
+														# read both of the switches
+		Switch_A = GPIO.input(self.Enc_A)
+		Switch_B = GPIO.input(self.Enc_B)
+														# now check if state of A or B has changed
+														# if not that means that bouncing caused it
+		if self.Current_A == Switch_A and self.Current_B == Switch_B:		# Same interrupt as before (Bouncing)?
+			return										# ignore interrupt!
+
+		self.Current_A = Switch_A								# remember new state
+		Current_B = Switch_B								# for next bouncing check
+		self.Current_B = Switch_B
+
+		if (Switch_A and Switch_B):						# Both one active? Yes -> end of sequence
+			LockRotary.acquire()						# get lock
+			if A_or_B == self.Enc_B:							# Turning direction depends on
+				self.Rotary_counter += 1						# which input gave last interrupt
+			else:										# so depending on direction either
+				self.Rotary_counter -= 1						# increase or decrease counter
+			LockRotary.release()						# and release lock
+		return											# THAT'S IT
 
 
 
@@ -186,5 +220,6 @@ def armKinimatics(x, y):
 	newCount1 = angleToCount(angle1, motor1CountableEvents)
 	newCount2 = angleToCount(angle2, motor2CountableEvents)
 	runMotors(newCount1, newCount2)
-
+encoder1 = Encoder(motor1ChannelA, motor1ChannelB)
+encoder2 = Encoder(motor2ChannelA, motor2ChannelB)
 armKinimatics(x, y)
