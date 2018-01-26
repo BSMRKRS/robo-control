a6ef89a96d9832857493c6edf2c8ea4299c56987
Jack <jwrickman18@bsmschool.org>
2018-01-26 13:49:35 -0600

    More beautification

--- ArmControl.py
+++ ArmControl.py
@@ -6,23 +6,23 @@
 #
 # Designed for a robotic arm with two encoded DC motors, elbow and shoulder.
 #
-# (0.) Initializes RaspberryPi, RoboPiLib, and other libraries. Holds all dependent
+# (0.) Initializes RaspberryPi, RoboPiLib, and other libraries. Holds all
 # variables based on hardware setup, pins, and other customizables.
 #
 # (1.) Takes a requested (x,y) coordinate in the arms range of motion
-# 	!!!Comment (1.) out if using ArmControl.py for iterating through (x,y) coords!!!
+# 	!!!Comment (1.) out if using ArmControl.py for iterating through (x,y)
 #
-# (2.) Translates (x,y) into the necessary angles of the arm beams for the endpoint to
+# (2.) Translates (x,y) into the necessary angles of the arm beams for the endp
 # be at (x,y)
 #
-# (3.) From there the angles are translated into the countable events from the encoders .
+# (3.) From there the angles are translated into the countable events from the
 #
-# (4.) The program then runs the motors until the counted events from the encoders is
+# (4.) The program then runs the motors until the counted events from the encod
 # equal to the requested number of events , moving the motors to the correct
 # angles, and moving the endpoint of the arm to the correct (x,y)
 # posistion.
 #
-# Assumes the starting position for the endpoint is (0,0) -- if it is not, find how many countable events
+# Assumes the starting position for the endpoint is (0,0) -- if it is not, find
 # motors start away from (0,0)
 #
 # Designed for Benilde St. Margaret's Rescue Robot, running on
@@ -42,9 +42,9 @@ import threading
 from time import sleep
 RPL.RoboPiInit("/dev/ttyAMA0", 115200)
 
-######### PINS ##########
+# PINS #
 
-### MOTOR 1 ###
+# MOTOR 1 #
 motor1Control = 0
 motor1EncoderPwrGnd = 2
 # GPIO pins, NOT on RoboPiHat
@@ -52,7 +52,7 @@ motor1ChannelA = 6
 motor1ChannelB = 12
 ###############
 
-### MOTOR 2 ###
+# MOTOR 2 #
 motor2Control = 4
 motor2EncoderPwrGnd = 5
 # GPIO pins, NOT on RoboPiHat
@@ -60,13 +60,13 @@ motor2ChannelA = 6
 motor2ChannelB = 7
 ###############
 
-#### PIN SETUP ####
+# PIN SETUP #
 RPL.pinMode(motor1Control, RPL.PWM)
 RPL.pinMode(motor2Control, RPL.PWM)
 ###################
 ##########################
 
-#### GLOBAL VARIABLES ####
+# GLOBAL VARIABLES #
 
 
 # Length of arms, from axle to axle
@@ -76,9 +76,12 @@ len2 = 12
 # for pwm motor control
 freq = 3000
 
-# Countable events per revolution of output shaft !!! This includes the motors internal gear ratio, and
-# the external gear ratio !!! Currently, the motor counts 5462.22 events per output revolution, and
-# is on a 16:1 ratio, so motor1CycleEvents = 5462.22 * 16 = 87395.52 events for one full revolution of the joint
+# Countable events per revolution of output shaft !!! This includes the motors
+# internal gear ratio, and
+# the external gear ratio !!! Currently, the motor counts 5462.22 events per
+# output revolution, and
+# is on a 16:1 ratio, so motor1CycleEvents = 5462.22 * 16 = 87395.52 events for
+# one full revolution of the joint
 motor1CycleEvents = 21848.88
 # Ditto motor1^^^, can be different if two motors have different encoders
 motor2CycleEvents = 11098.56
@@ -87,18 +90,19 @@ motor2CycleEvents = 11098.56
 ################################
 # 1. USER INPUT
 ################################
-
+"""
 # Takes x,y coordinate pair for the arm's endpoint requested destination
-# If this program is to be used to increment through (x,y) coordinates along a plane/automatically,
-# comment this section out, and increment through armKinimatics(x,y) using an outside program to increment through
-# values.
+If this program is to be used to increment through (x,y) coordinates along a
+plane/automatically comment this section out, and increment through
+armKinimatics(x,y) using an outside program to increment through values.
+"""
 x = float(raw_input("x>"))
 y = float(raw_input("y>"))
 
 ################################
 # 2. INVERSE KINIMATICS
 ###############################
-# inverse_kinimatic takes an x,y coordinate and returns it as two angles, a1 and a2
+# inverse_kinimatic takes an x,y coordinate and returns it as two angles
 
 
 def LawOfCosines(a, b, c):
@@ -142,7 +146,7 @@ def angleToCount(angle, motorXCycleEvents):
 def runMotors(newCount1, newCount2, encoder1, encoder2):
     global motor1Control, motor2Control, freq
     count1 = encoder1.Rotary_counter
-    count2 = encoder2.Rotary_counter		# Starts Motor1 and Motor2 in correct direction
+    count2 = encoder2.Rotary_counter		# Starts Motor1 and Motor2
     if newCount1 > count1:
         RPL.pwmWrite(motor1Control, 2500, freq)
     elif newCount1 < count1:
@@ -180,7 +184,7 @@ class Encoder(object, Enc_A, Enc_B):
     def __init__(self):
         self.Enc_A = Enc_A  # GPIO encoder pin A
         self.Enc_B = Enc_B  # GPIO encoder pin B
-        self.Current_A = 1  # This assumes that Encoder is initaiate while the motor is not moving
+        self.Current_A = 1  # This assumes that Encoder initiates while mtr sto
         self.Current_B = 1
         self.Rotary_counter = 0
         self.startEncoders()
@@ -194,17 +198,17 @@ class Encoder(object, Enc_A, Enc_B):
         # setup callback thread for the A and B encoder
         # use interrupts for all inputs
         GPIO.add_event_detect(self.Enc_A, GPIO.RISING,
-                              callback=self.rotary_interrupt) 				# NO bouncetime
+                              callback=self.rotary_interrupt) 	# NO bouncetime
         GPIO.add_event_detect(self.Enc_B, GPIO.RISING,
-                              callback=self.rotary_interrupt) 				# NO bouncetime
+                              callback=self.rotary_interrupt) 	# NO bouncetime
         return
 
     def rotary_interrupt(self, A_or_B):
-                                                                                                        # read both of the switches
+                                                    # read both of the switches
         Switch_A = GPIO.input(self.Enc_A)
         Switch_B = GPIO.input(self.Enc_B)
         # now check if state of A or B has changed
-        # if not that means that bouncing caused it Same interrupt as before (Bouncing)?
+        # if not that means that bouncing caused it Same interrupt as before
         if self.Current_A == Switch_A and self.Current_B == Switch_B:
             return										# ignore interrupt!
 
@@ -212,7 +216,7 @@ class Encoder(object, Enc_A, Enc_B):
         Current_B = Switch_B							# for next bouncing check
         self.Current_B = Switch_B
 
-        if (Switch_A and Switch_B):						# Both one active? Yes -> end of sequence
+        if (Switch_A and Switch_B):		# Both one active? Yes -> end of sequence
             LockRotary.acquire()						# get lock
             if A_or_B == self.Enc_B:					# Turning direction depends on
                 self.Rotary_counter += 1				# which input gave last interrupt
