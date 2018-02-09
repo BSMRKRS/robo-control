2296e3d04f871b77d8e83f43aef83a2eb37743c9
Jack <jwrickman18@bsmschool.org>
2018-02-08 23:35:27 -0600

    Moved all inverkin class to not calss b/c stupid

--- ArmControlLib.py
+++ ArmControlLib.py
@@ -119,54 +119,60 @@ class Motor(object):
 ############################
 
 
-class Inverse_Kinimatics():
-    global len1, len2
+global len1, len2
 
-    def LawOfCosines(a, b, c):
-        C = math.acos((a * a + b * b - c * c) / (2 * a * b))
-        return C
 
-    def distance(x, y):
-        return math.sqrt(x * x + y * y)
+def LawOfCosines(a, b, c):
+    C = math.acos((a * a + b * b - c * c) / (2 * a * b))
+    return C
 
-    def inverse_kinimatic(x, y):
-        len1 = 12.0
-        len2 = 12.0  # TODO: Fix hardwrite of length
-        dist = distance(x, y)
-        D1 = math.atan2(y, x)
-        D2 = LawOfCosines(dist, len1, len2)
-        A1 = D1 + D2
-        B2 = LawOfCosines(len1, len2, dist)
-        return A1, B1
 
-    def deg(rad):
-        return rad * 180 / math.pi
+def distance(x, y):
+    return math.sqrt(x * x + y * y)
 
-    def armKinimatics(x, y):
-        angle1, angle2 = Inverse_Kinimatics.angle(x, y)
-        newCount1 = Inverse_Kinimatics.angleToCount(angle1, motor1.cycleEvents)
-        newCount2 = Inverse_Kinimatics.angleToCount(angle2, motor2.cycleEvents)
-        Inverse_Kinimatics.runMotors(newCount1, newCount2)
 
-    def angleToCount(angle, motorXCycleEvents):
-        CycleEventsPerDegree = motorXCycleEvents / 360
-        count = angle * CycleEventsPerDegree
-        return count
+def inverse_kinimatic(x, y):
+    len1 = 12.0
+    len2 = 12.0  # TODO: Fix hardwrite of length
+    dist = distance(x, y)
+    D1 = math.atan2(y, x)
+    D2 = LawOfCosines(dist, len1, len2)
+    A1 = D1 + D2
+    B2 = LawOfCosines(len1, len2, dist)
+    return A1, B1
 
-    def runMotors(newCount1, newCount2):
-        global motor1, motor2, freq
-        motor1.move_to_position(newCount1)  # Starts Motor1
-        motor2.move_to_position(newCount2)  # Starts Motor2
-        a = True
-        b = True
-        while a or b:
 
-            if abs(newCount1 - motor1.encoder.Rotary_counter) < 5:
-                motor1.stop()
-                a = False
-                print "Motor 1 complete"
+def deg(rad):
+    return rad * 180 / math.pi
 
-            if abs(newCount2 - motor2.encoder.Rotary_counter) < 5:
-                motor2.stop()
-                b = False
-                print "Motor 2 complete"
+
+def armKinimatics(x, y):
+    angle1, angle2 = angle(x, y)
+    newCount1 = angleToCount(angle1, motor1.cycleEvents)
+    newCount2 = angleToCount(angle2, motor2.cycleEvents)
+    runMotors(newCount1, newCount2)
+
+
+def angleToCount(angle, motorXCycleEvents):
+    CycleEventsPerDegree = motorXCycleEvents / 360
+    count = angle * CycleEventsPerDegree
+    return count
+
+
+def runMotors(newCount1, newCount2):
+    global motor1, motor2, freq
+    motor1.move_to_position(newCount1)  # Starts Motor1
+    motor2.move_to_position(newCount2)  # Starts Motor2
+    a = True
+    b = True
+    while a or b:
+
+        if abs(newCount1 - motor1.encoder.Rotary_counter) < 5:
+            motor1.stop()
+            a = False
+            print "Motor 1 complete"
+
+        if abs(newCount2 - motor2.encoder.Rotary_counter) < 5:
+            motor2.stop()
+            b = False
+            print "Motor 2 complete"
