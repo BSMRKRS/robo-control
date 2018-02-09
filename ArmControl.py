57ad267c4d8696eb4f5026051c66bf186257cd41
Jack <jwrickman18@bsmschool.org>
2018-02-08 23:41:29 -0600

    Readded angle cause it got lost somehow

--- ArmControl.py
+++ ArmControl.py
@@ -87,9 +87,23 @@ def runMotors(newCount1, newCount2):
             print "Motor 2 complete"
 
 
+def angles(x, y):
+    dist = distance(x, y)
+
+    D1 = math.atan2(y, x)
+
+    D2 = LawOfCosines(dist, len1, len2)
+
+    A1 = D1 + D2
+
+    A2 = LawOfCosines(len1, len2, dist)
+
+    return A1, A2
+
 ################################
 # EXECUTE
 ################################
+
 
 x = float(raw_input("x>"))
 y = float(raw_input("y>"))
