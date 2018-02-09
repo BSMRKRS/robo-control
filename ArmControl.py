a930b10ee9dd9d35c0f456ebd5dc2f47b7619c3a
Jack <jwrickman18@bsmschool.org>
2018-02-08 23:38:21 -0600

    Moved stuff out of ARMCONTROLLIb

--- ArmControl.py
+++ ArmControl.py
@@ -32,6 +32,61 @@ len2 = 12
 freq = 3000
 
 
+def LawOfCosines(a, b, c):
+    C = math.acos((a * a + b * b - c * c) / (2 * a * b))
+    return C
+
+
+def distance(x, y):
+    return math.sqrt(x * x + y * y)
+
+
+def inverse_kinimatic(x, y):
+    global len1, len2   # TODO: Fix hardwrite of length
+    dist = distance(x, y)
+    D1 = math.atan2(y, x)
+    D2 = LawOfCosines(dist, len1, len2)
+    A1 = D1 + D2
+    B2 = LawOfCosines(len1, len2, dist)
+    return A1, B1
+
+
+def deg(rad):
+    return rad * 180 / math.pi
+
+
+def armKinimatics(x, y):
+    angle1, angle2 = angle(x, y)
+    newCount1 = angleToCount(angle1, motor1.cycleEvents)
+    newCount2 = angleToCount(angle2, motor2.cycleEvents)
+    runMotors(newCount1, newCount2)
+
+
+def angleToCount(angle, motorXCycleEvents):
+    CycleEventsPerDegree = motorXCycleEvents / 360
+    count = angle * CycleEventsPerDegree
+    return count
+
+
+def runMotors(newCount1, newCount2):
+    global motor1, motor2, freq
+    motor1.move_to_position(newCount1)  # Starts Motor1
+    motor2.move_to_position(newCount2)  # Starts Motor2
+    a = True
+    b = True
+    while a or b:
+
+        if abs(newCount1 - motor1.encoder.Rotary_counter) < 5:
+            motor1.stop()
+            a = False
+            print "Motor 1 complete"
+
+        if abs(newCount2 - motor2.encoder.Rotary_counter) < 5:
+            motor2.stop()
+            b = False
+            print "Motor 2 complete"
+
+
 ################################
 # EXECUTE
 ################################
@@ -48,5 +103,6 @@ motor1 = ACL.Motor(0, 1, 19, 16, 1000, 1000, 21848.88, freq)
 
 ## Motor2 ##
 motor2 = ACL.Motor(2, 3, 26, 20, 1000, 1000, 11098.56, freq)
+
 
 ACL.armKinimatics(x, y, motor1, motor2)
