5f7966a27af8d5bd000dd73e846ea649044a1b92
Jack <jwrickman18@bsmschool.org>
2018-02-22 23:28:41 -0600

    Variable error

--- clientRead.py
+++ clientRead.py
@@ -32,10 +32,10 @@ while True:
     convertTxtArray = buff.split()
     motor1_count_request = float(convertTxtArray[0])
     motor2_count_request = float(convertTxtArray[1])
-    print "Motor1 newCount: %d" % newCount1
-    print "Motor2 newCount: %d" % newCount2
-    motor1.move_to_position(newCount1)  # Starts Motor1
-    motor2.move_to_position(newCount2)  # Starts Motor2
+    print "Motor1 newCount: %d" % motor1_count_request
+    print "Motor2 newCount: %d" % motor2_count_request
+    motor1.move_to_position(motor1_count_request)  # Starts Motor1
+    motor2.move_to_position(motor2_count_request)  # Starts Motor2
     a = True
     b = True
     timeStart = time.time()
@@ -44,12 +44,12 @@ while True:
         if time.time() - timeStart > 1:
             print "Motor1 rot count: %d Motor2 rot count: %d" % (
                 motor1.encoder.Rotary_counter, motor2.encoder.Rotary_counter)
-        if abs(newCount1 - motor1.encoder.Rotary_counter) < 5:
+        if abs(motor1_count_request - motor1.encoder.Rotary_counter) < 5:
             motor1.stop()
             a = False
             print "Motor 1 complete"
 
-        if abs(newCount2 - motor2.encoder.Rotary_counter) < 5:
+        if abs(motor2_count_request - motor2.encoder.Rotary_counter) < 5:
             motor2.stop()
             b = False
             print "Motor 2 complete"
