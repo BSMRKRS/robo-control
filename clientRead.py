a2368aed38d467be1cf2dad7ded51d03e9fbc6b0
Jack <jwrickman18@bsmschool.org>
2018-02-22 23:09:25 -0600

    MOve on greater abs value, instead of stop on less. also, print moto

--- clientRead.py
+++ clientRead.py
@@ -17,7 +17,8 @@ motor1 = ACL.Motor(0, 1, 26, 20, 1000, 1000, 21848.88, freq)
 
 ## Motor2 ##
 motor2 = ACL.Motor(2, 3, 19, 16, 1000, 1000, 11098.56, freq)
-
+motor1CompletePrint = True
+motor2CompletePrint = True
 while True:
     gFile = open("ftpTemp.txt", "wb")
     ftp.retrbinary('RETR ftpTemp.txt', gFile.write)
@@ -29,13 +30,17 @@ while True:
     motor1_count_request = float(convertTxtArray[0])
     motor2_count_request = float(convertTxtArray[1])
     print "Latency: %f" % (time.time() - float(convertTxtArray[2]))
-    if abs(motor1_count_request - motor1.encoder.Rotary_counter) < 10:
-        motor1.stop()
-        print "motor1 complete"
-    else:
+    if abs(motor1_count_request - motor1.encoder.Rotary_counter) > 10:
         motor1.move_to_position(motor1_count_request)
-    if abs(motor2_count_request - motor2.encoder.Rotary_counter) < 10:
-        motor2.stop()
-        print "motor2 complete"
     else:
+        motor1.stop()
+        if motor1CompletePrint:
+            print "Motor1 Complete"
+            motor1CompletePrint = False
+    if abs(motor2_count_request - motor2.encoder.Rotary_counter) > 10:
         motor2.move_to_position(motor2_count_request)
+    else:
+        motor2.stop()
+        if motor2CompletePrint:
+            print "Motor2 Complete"
+            motor2CompletePrint = False
