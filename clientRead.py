2e6aed48cbf2d38e0cf6ed29cb9e080f5d6edb98
Jack <jwrickman18@bsmschool.org>
2018-02-24 00:27:13 -0600

    Using a Inverse Kinimatic runMotors to control motor

--- clientRead.py
+++ clientRead.py
@@ -21,7 +21,7 @@ motor1 = ACL.Motor(0, 1, 26, 20, 1000, 1000, 21848.88, freq)
 ## Motor2 ##
 motor2 = ACL.Motor(2, 3, 19, 16, 1000, 1000, 11098.56, freq)
 
-#IKI = ACL.Inverse_Kinimatics(12.0, 12.0, motor1, motor2)
+IKI = ACL.Inverse_Kinimatics(12.0, 12.0, motor1, motor2)
 
 motor1CompletePrint = True
 motor2CompletePrint = True
@@ -45,13 +45,6 @@ while True:
         timeStart = time.time()
     if motor1_count_request_new != motor1_count_request_old or motor2_count_request_new != motor2_count_request_old:
         print "New Command Recieved"
-        motor1.move_to_position(motor1_count_request_new)
-        motor2.move_to_position(motor2_count_request_new)
+        IKI.runMotors(motor1_count_request_new, motor2_count_request_new)
         motor1_count_request_old = motor1_count_request_new
         motor2_count_request_old = motor2_count_request_new
-    if abs(motor1_count_request_new - motor1.encoder.Rotary_counter) < 10:
-        motor1.stop()
-        print "Motor1 Stopped"
-    if abs(motor2_count_request_new - motor2.encoder.Rotary_counter) < 10:
-        motor2.stop()
-        print "Motor2 Stopped"
