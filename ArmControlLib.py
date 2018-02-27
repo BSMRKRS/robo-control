0dd2c65afabef21952353e558c9345964a62f5e7
Jack <jwrickman18@bsmschool.org>
2018-02-27 00:32:51 -0600

    More reversals

--- ArmControlLib.py
+++ ArmControlLib.py
@@ -137,9 +137,9 @@ class Inverse_Kinimatics(object):
     def __init__(self, len1, len2, motor1, motor2):
         self.len1 = len1
         self.len2 = len2
-        self.data = [0, 0]
         self.motor1 = motor1
         self.motor2 = motor2
+        self.data = [0, 0]
 
     def LawOfCosines(self, a, b, c):
         C = math.acos((a * a + b * b - c * c) / (2 * a * b))
