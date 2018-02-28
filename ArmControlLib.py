9e8f3ccc8d36c152016bfc40a870b014598a3ae4
Jack <jwrickman18@bsmschool.org>
2018-02-28 00:28:26 -0600

    Printout at end of runMotors

--- ArmControlLib.py
+++ ArmControlLib.py
@@ -201,6 +201,7 @@ class Inverse_Kinimatics(object):
                 self.motor2.stop()
                 time.sleep(0.01)
                 b = False
+        print "Move Complete"
 
     def angle(self, x, y):
         len1 = self.len1
