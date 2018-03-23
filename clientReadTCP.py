124a591fbb156edcc4a7fa7d1547176f395e4a54
Jack <jwrickman18@bsmschool.org>
2018-03-23 01:55:45 -0500

    A version of clientRead.py that utiliizes TCP protocal

---
+++ clientReadTCP.py
@@ -0,0 +1,44 @@
+# --------------------------------File on Robot---------------------------------
+# Hosts a TCP connection and interprets the data recieved
+import ArmControlLib as ACL
+import sys
+import os
+import sys
+import os
+import socket
+import RoboPiLib_pwm as RPL
+RPL.RoboPiInit("/dev/ttyAMA0", 115200)
+
+######################
+##    Host Info     ##
+######################
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address given on the command line
+host = sys.argv[1]
+server_address = (host, 10000)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+sock.listen(1)
+
+######################
+##      Main        ##
+######################
+while True:
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+    try:
+        print >>sys.stderr, 'client connected:', client_address
+        while True:
+            data = connection.recv(9)
+            data = data.split(' ')
+            if time.time() - time_stamp > 0.1:
+                time_stamp = time.time()
+            else:
+                motor1.move_to_position(int(motor1_count_request_new))
+                time.sleep(0.001)
+                motor2.move_to_position(int(motor2_count_request_new))
+
+    finally:
+        connection.close()
