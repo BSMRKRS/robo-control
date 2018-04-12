d38455605293552ffd52d376d09a7ac1c3ca7820
Jack <jwrickman18@bsmschool.org>
2018-04-12 12:43:43 -0500

    fdst

--- clientReadTCP.py
+++ clientReadTCP.py
@@ -53,7 +53,7 @@ while True:
                 motor1.clockwise()
             elif data[0] == 2:
                 motor1.counterClockwise()
-            time.sleep(0.1))
+            time.sleep(0.1)
 
     finally:
         connection.close()
